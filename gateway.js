407319d9c0978599ad8c1c6cf1de0d45855216a5
Muhammad327 <muhammadkh303@gmail.com>
2024-01-08 16:01:46 +0500

    modification in gateway file

--- gateway.js
+++ gateway.js
@@ -4,11 +4,12 @@ const exec=require('child_process').exec
 function getGateway(callback){
   return new Promise((resolve,reject)=>{
     exec("ipconfig /all", function (err, stdout, stderr) {
-        if (err) {
-            reject(err);
-            return;
-        }
-        
+      if (err) {
+        reject(err);
+        return;
+      }
+        console.log('.....run.....');
+              
         const lines = stdout.split('\n');
         const defaultGatewayLine = lines.find((line) => line.includes('Default Gateway'));
         const defaultGatewayArray=defaultGatewayLine.split(':');
